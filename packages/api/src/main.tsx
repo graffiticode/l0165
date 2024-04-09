58099fa8632e2fd4d945e00637ba0b6432d1c484
Jeff Dyer <jeffdyer@acm.org>
2024-04-09 15:59:27 -0700

    s/server/api/ s/client/web/

--- packages/api/src/main.tsx
+++ packages/api/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { View } from '../../client/dist/index.es.js';
-import '../../client/dist/style.css';
+import { View } from '../../web/dist/index.es.js';
+import '../../web/dist/style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
