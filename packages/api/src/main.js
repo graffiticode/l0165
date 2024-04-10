fe7f533009df9ba6b59c7c560b61d7cd3988bbe9
Jeff Dyer <jeffdyer@acm.org>
2024-04-10 16:31:07 -0700

    Add better logging of authUrl

--- packages/api/src/main.js
+++ packages/api/src/main.js
@@ -4,10 +4,9 @@ import { createApp } from "./app.js";
 
 const port = process.env.PORT || "50002";
 const authUrl = process.env.AUTH_URL || "https://auth.graffiticode.org";
-console.log("AUTH_URL=" + process.env.AUTH_URL);
-
 const app = createApp({ authUrl });
 app.listen(port, () => {
+  console.log(`Using authUrl ${process.env.AUTH_URL}`);
   console.log(`Listening on ${port}...`);
 });
 
