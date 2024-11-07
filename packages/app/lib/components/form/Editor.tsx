dd748135c45c8bf870cd78cd9273a8e03a74b850
Jeff Dyer <jeffdyer@acm.org>
2024-11-07 11:10:59 -0800

    Add placeholder for table editor

--- packages/app/lib/components/form/Editor.tsx
+++ packages/app/lib/components/form/Editor.tsx
@@ -3,10 +3,15 @@ import { MenuView } from "./MenuView";
 import { TextEditor } from "./TextEditor";
 
 export const Editor = ({ state }) => {
+  const { type } = state.data;
   return (
     <div>
       <MenuView className="hidden" editorView={null} />
-      <TextEditor state={state} />
+      {
+        type === "table" && <div>Table Editor goes here.</div> ||
+          type === "text" && <TextEditor state={state} /> ||
+          <div />
+      }
     </div>
   );
 };
