454e4657f260bdbd5691fcbe7388673cbabcb28c
Jeff Dyer <jeffdyer@acm.org>
2024-12-31 12:03:42 -0800

    Refactor cellPlugin/state/apply()

--- packages/app/lib/components/form/TableEditor.tsx
+++ packages/app/lib/components/form/TableEditor.tsx
@@ -1,5 +1,6 @@
 /*
   TODO
+  [ ] FIXME when no longer editing a cell, add it to the dirty list
   [ ] Make row and column headings read only. Focus on first cell
   [ ] Format numbers and dates using format patterns
   [ ] Handle single and double click and tab in cells
@@ -49,6 +50,7 @@ import {
   goToNextCell,
   //   deleteTable,
   //findCell,
+  TableMap,
 } from "prosemirror-tables";
 import {
   tableEditing,
@@ -298,33 +300,77 @@ const schema = new Schema({
   marks: baseSchema.spec.marks,
 });
 
-const getCellNodeByName = ({doc, name}) => {
-  let result;
-  doc.descendants((node, pos) => {
-    if (result !== undefined) {
-      return false;
+const findTable = $pos => {
+  for (let depth = $pos.depth; depth > 0; depth--) {
+    const node = $pos.node(depth);
+    if (node.type.name === "table") {
+      return { pos: $pos.before(depth), node };
     }
-    if (isTableCellOrHeader(node) && node.attrs.name === name) {
-      result = {node, pos};
-    }
-  });
-  result = result || {};
-  return result;
+  }
+  return null;
+}
+
+const getTablePosition = state => {
+  const { $from } = state.selection;
+  const table = findTable($from);
+  return table ? table.pos : null;
+}
+
+const letters = "_ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+const getCellRowColFromName = name => {
+  const colName = name.slice(0, 1);
+  const rowName = name.slice(1);
+  const col = letters.indexOf(colName);
+  const row = +rowName;
+  return {row, col};
 };
+
+const getCellNodeByName = ({ state, name }) => {
+  const tablePos = getTablePosition(state);
+  const tableNode = state.doc.nodeAt(tablePos);
+  const tableMap = TableMap.get(tableNode);
+  const { row, col } = getCellRowColFromName(name);
+  const cellIndex = row * tableMap.width + col;
+  const cellPos = tableMap.map[cellIndex] + tablePos + 1;
+  const cellNode = state.doc.nodeAt(cellPos);
+  return {node: cellNode, pos: cellPos};
+}
+
+// const getCellNodeByName = ({doc, name}) => {
+//   let result;
+//   doc.descendants((node, pos) => {
+//     if (result !== undefined) {
+//       return false;
+//     }
+//     if (isTableCellOrHeader(node) && node.attrs.name === name) {
+//       result = {node, pos};
+//     }
+//   });
+//   result = result || {};
+//   return result;
+// };
 
 const replaceCellContent = (editorView, name, newText, doMoveCursor = false) => {
   const { state, dispatch } = editorView;
-  const { pos: cellPos } = getCellNodeByName({doc: state.doc, name});
-  const cellNode = state.doc.nodeAt(cellPos);
+  const { pos: cellPos, node: cellNode } = getCellNodeByName({state, name});
   if (!cellNode || !isTableCellOrHeader(cellNode)) {
     console.error("Invalid cell position or node type: " + JSON.stringify(cellNode, null, 2));
     return;
   }
+  // FIXME verify that this is the correct position.
   const contentStart = cellPos + 1;
   const contentEnd = cellPos + cellNode.nodeSize - 1;
   const paragraphNode = newText &&
         state.schema.node("paragraph", null, state.schema.text(newText)) ||
         state.schema.node("paragraph");
+  // console.log(
+  //   "replaceCellContent()",
+  //   "name=" + name,
+  //   "newText=" + newText,
+  //   "cellNode=" + JSON.stringify(cellNode, null, 2),
+  //   "paragraphNode=" + JSON.stringify(paragraphNode, null, 2)
+  // );
   const tr = state.tr;
   tr.replaceWith(contentStart, contentEnd, paragraphNode);
   if (doMoveCursor) {
@@ -528,7 +574,16 @@ const cellPlugin = new Plugin({
             val,
           };
           const formattedVal = formatCellValue({env: {cells}, name});
-          const { node } = getCellNodeByName({doc: view.state.doc, name});
+          const { node } = getCellNodeByName({state: view.state, name});
+          // console.log(
+          //   "[1] cellPlugin/view/update()",
+          //   "name=" + name,
+          //   "focusedCell=" + pluginState.focusedCell,
+          //   "textContent=" + node.textContent,
+          //   "formattedVal=" + formattedVal,
+          //   "name !== pluginState.focusedCell=" + (name !== pluginState.focusedCell),
+          //   "formattedVal !== node.textContent=" + (formattedVal !== node.textContent)
+          // );
           if (name !== pluginState.focusedCell && formattedVal !== node.textContent) {
             replaceCellContent(view, name, formattedVal);
           }
@@ -536,7 +591,13 @@ const cellPlugin = new Plugin({
         if (pluginState.focusedCell) {
           const name = pluginState.focusedCell;
           const text = pluginState.cells[name]?.text || "";
-          const { node } = getCellNodeByName({doc: view.state.doc, name});
+          const { node } = getCellNodeByName({state: view.state, name});
+          // console.log(
+          //   "[2] cellPlugin/view/update()",
+          //   "name=" + name,
+          //   "text=" + text,
+          //   "node=" + JSON.stringify(node, null, 2)
+          // );
           if (node.type.name === "table_cell" && text !== node.textContent) {
             replaceCellContent(view, name, text, true);
           }
@@ -592,7 +653,7 @@ const cellPlugin = new Plugin({
         }
       }, cells);
       // console.log(
-      //   "cellsPugin/init()",
+      //   "cellsPugin/state/init()",
       //   "cellsWithDeps=" + JSON.stringify(cellsWithDeps, null, 2)
       // );
       return {
@@ -607,9 +668,6 @@ const cellPlugin = new Plugin({
       tr = tr;
       oldState = oldState;
       newState = newState;
-      const { selection } = newState;
-      const $anchor = selection.$anchor;
-      let node = $anchor.node(-1);
       if (tr.getMeta("updated")) {
         value = {
           ...value,
@@ -617,110 +675,98 @@ const cellPlugin = new Plugin({
           dirtyCells: [],
         };
       }
-      if (node && isTableCellOrHeader(node)) {
-        const name = node.attrs.name;
-        if (value.lastFocusedCell !== name) {
-          // We just left a cell, so recompute its value and the values of its
-          // dependencies.
-          if (value.lastFocusedCell) {
-            const { lastFocusedCell } = value;
-            const cell = value.cells[lastFocusedCell];
-            value = {
+      const { selection } = newState;
+      const $anchor = selection.$anchor;
+      const node = $anchor.node(-1);
+      const name = node.attrs?.name;
+      const lastFocusedCell = value.lastFocusedCell;
+      if (lastFocusedCell !== name) {
+        // We just left a cell, so recompute its value and the values of its
+        // dependencies.
+        if (lastFocusedCell) {
+          //const { lastFocusedCell } = value;
+          const cell = value.cells[lastFocusedCell];
+          value = {
+            ...value,
+            blurredCell: lastFocusedCell,
+            cells: {
+              ...value.cells,
+              [lastFocusedCell]: {
+                ...cell,
+                val: evalCell({env: value, name: lastFocusedCell}),
+              },
+            },
+            dirtyCells: [
+              ...value?.dirtyCells,
+              lastFocusedCell,  // Order matters.
+              ...(cell.deps || []),
+            ],
+          };
+          const deps = getCellDependencies({env: value, names: [lastFocusedCell]});
+          value = deps.reduce((value, name) => {
+            // Add current cell as dependency of independent cells.
+            const cell = value.cells[name];
+            return cell && {
               ...value,
-              blurredCell: lastFocusedCell,
               cells: {
                 ...value.cells,
-                [lastFocusedCell]: {
+                [name]: {
                   ...cell,
-                  val: evalCell({env: value, name: lastFocusedCell}),
+                  val: evalCell({env: value, name}),
+                  deps: [
+                    ...cell?.deps,
+                    ...!cell.deps.includes(lastFocusedCell) && [lastFocusedCell] || [],
+                  ],
                 },
               },
-              dirtyCells: [
-                // FIXME if the text hasn't changed then don't amend dirtyCells.
-                ...value?.dirtyCells,
-                lastFocusedCell,  // Order matters.
-                ...(cell.deps || []),
-              ],
+            } || value;
+          }, value);
+          if (cell.deps?.includes(name)) {
+            // If the focusedCell depends on the lastFocusedCell, update its
+            // its so its dependents are updated.
+            const val = evalCell({ env: value, name });
+            value = {
+              ...value,
+              cells: {
+                ...value.cells,
+                [name]: {
+                  ...value.cells[name],
+                  val: val || undefined,
+                },
+              },
             };
-            const deps = getCellDependencies({env: value, names: [lastFocusedCell]});
-            value = deps.reduce((value, name) => {
-              // Add current cell as dependency of independent cells.
-              const cell = value.cells[name];
-              return cell && {
-                ...value,
-                cells: {
-                  ...value.cells,
-                  [name]: {
-                    ...cell,
-                    val: evalCell({env: value, name}),
-                    deps: [
-                      ...cell?.deps,
-                      ...!cell.deps.includes(lastFocusedCell) && [lastFocusedCell] || [],
-                    ],
-                  },
-                },
-              } || value;
-            }, value);
-            if (cell.deps?.includes(name)) {
-              // If the focusedCell depends on the lastFocusedCell, update its
-              // its so its dependents are updated.
-              const val = evalCell({ env: value, name });
-              value = {
-                ...value,
-                cells: {
-                  ...value.cells,
-                  [name]: {
-                    ...value.cells[name],
-                    val: val || undefined,
-                  },
-                },
-              };
-            }
           }
-          value = {
-            ...value,
-            lastFocusedCell: node.attrs.name,
-            focusedCell: node.attrs.name,
-          };
-        } else if (name) {
-          const text = node.textContent.trim();
-          // console.log(
-          //   "cellPlugin/apply()",
-          //   "text=" + text,
-          //   "value=" + JSON.stringify(value, null, 2)
-          // );
-          value = {
-            ...value,
-            blurredCell: null,
-            focusedCell: null,
-            dirtyCells: [],
-            cells: {
-              ...value.cells,
-              [name]: {
-                ...value.cells[name],
-                text: text || undefined,
-              },
-            },
-          };
-          const val = evalCell({env: value, name});
-          value = {
-            ...value,
-            blurredCell: null,
-            focusedCell: null,
-            dirtyCells: [],
-            cells: {
-              ...value.cells,
-              [name]: {
-                ...value.cells[name],
-                text: text || undefined,
-                val: val || undefined,
-              },
-            },
-          };
         }
+        value = {
+          ...value,
+          lastFocusedCell: node.attrs.name,
+          focusedCell: node.attrs.name,
+        };
+        // console.log(
+        //   "[2] cellPlugin/state/apply()",
+        //   "value=" + JSON.stringify(value, null, 2)
+        // );
+      } else if (isTableCellOrHeader(node) && node.attrs?.name) {
+        const name = node.attrs.name;
+        const text = node.textContent.trim();
+        const val = evalCell({env: value, name});
+        value = {
+          ...value,
+          blurredCell: null,
+          focusedCell: null,
+          dirtyCells: [],
+          cells: {
+            ...value.cells,
+            [name]: {
+              ...value.cells[name],
+              text: text || undefined,
+              val: val || undefined,
+            },
+          },
+        };
       }
       // console.log(
-      //   "cellPlugin/apply()",
+      //   "cellPlugin/state/apply()",
       //   "value=" + JSON.stringify(value, null, 2)
       // );
       return value;
@@ -821,7 +867,7 @@ export const TableEditor = ({ state }) => {
         plugins,
       }, editorState);
       editorView.updateState(newEditorState);
-      const { pos } = getCellNodeByName({doc: newEditorState.doc, name: "A1"});
+      const { pos } = getCellNodeByName({state: newEditorState, name: "A1"});
       if (!pos) return;
       const resolvedPos = newEditorState.doc.resolve(pos);
       editorView.dispatch(editorView.state.tr.setSelection(new TextSelection(resolvedPos)));
